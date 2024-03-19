db1b0d0cb80724a808418e11d8f78dcbb0312b29
cn1adil24 <shehmeeradil123@gmail.com>
2024-03-19 23:21:37 +0100

    Add bean for generating data for inventory service

--- inventory-service/src/main/java/dev/adil/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/dev/adil/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,12 @@
 package dev.adil.inventoryservice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import dev.adil.inventoryservice.model.Inventory;
+import dev.adil.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -9,5 +14,20 @@ public class InventoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
-
+	
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args -> {
+			Inventory inventory1 = new Inventory();
+			inventory1.setSkuCode("iphone_13");
+			inventory1.setQuantity(100);
+			
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("iphone_13_red");
+			inventory2.setQuantity(0);
+			
+			inventoryRepository.save(inventory1);
+			inventoryRepository.save(inventory2);
+		};
+	}
 }
